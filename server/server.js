f520299ada5c6100dad00b6b8150c496cd832e5f
manazir <ahsanmanaz@gmail.com>
2025-06-20 22:07:36 +0400

    origin url added

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB();
 await connectCloudinary();
 
 // allow origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://mern-stack-jet.vercel.app","http://localhost:3000"];
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
